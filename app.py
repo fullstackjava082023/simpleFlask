000169aaa31a5e52e7303a6fdde27f8f1faa9080
iliap <iliap@sapiens.com>
2024-09-09 21:48:54 +0300

    reanme app.py

---
+++ app.py
@@ -0,0 +1,16 @@
+# craete simple flask app
+from flask import Flask
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000, host='0.0.0.0')
