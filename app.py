7d478b7880f21634b0c6e52384558c8edf1083ce
Vagrant User <vagrant@cleanUbuntuJenkins>
2024-09-24 10:22:44 +0300

    added code to extract things from webhook

--- app.py
+++ app.py
@@ -13,23 +13,36 @@ def hello_world():
     global request_counter
     request_counter += 1
     hostname = request.headers.get('Host')
-     # Get the real client IP from the X-Forwarded-For header
-    real_ip = request.headers.get('X-Forwarded-For', request.remote_addr)
-    return f'''Hello, World! my server is: {hostname} and request count is: {request_counter}
-            the Machine IP address is: {real_ip}
-            '''
+    return f'Hello, World! my server is: {hostname} and request count is: {request_counter}'
+
+
+
 
 
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    print(f"Received webhook data ", flush=True)
+    print(f"Received webhook data", flush=True)
     webhook_data = request.get_json()
+
     if webhook_data:
+        # Extract only repository name and pusher name
+        repository_name = webhook_data.get('repository', {}).get('name', 'Unknown repository')
+        pusher_name = webhook_data.get('pusher', {}).get('name', 'Unknown pusher')
+
+        # Store only the repository name and pusher name
+        filtered_data = {
+            'repository_name': repository_name,
+            'pusher_name': pusher_name
+        }
+
+        # Save the filtered data to a JSON file
         with open('/opt/simpleFlask/webhook_data.json', 'w') as f:
-            json.dump(webhook_data, f, indent=4)
-        return 'Webhook received and saved', 200
-    return 'Webhook received', 200
+            json.dump(filtered_data, f, indent=4)
+
+        return 'Webhook received and filtered data saved', 200
+
+    return 'Webhook received but no data', 200
 
 
 # route to show webhook data
